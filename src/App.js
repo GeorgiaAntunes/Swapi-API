1f7d22291ef15fe39dc76f0d90bcde6dada4d036
Georgia Antunes <georgia.ilegra@terceiros.zenvia.com>
2022-05-23 14:40:17 -0300

    Home Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-// import People from './components/People';
-// import Planets from './components/Planets';
 import './App.css';
 import { BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 import People from './components/People';
@@ -9,30 +7,6 @@ import Home from './components/Home';
 import Planets from './components/Planets';
 
 function App() {
- 
-  // // const [loading, setloading] = useState(true);
-
-  // useEffect(() => {
-  //   // async function fetchPeople() {
-  //   //   let res = await fetch('https://swapi.dev/api/people/?=format=json');
-  //   //   let data = await res.json();
-  //   //   setPeople(data.results);
-  //   // }
-
-  //   async function fetchPlanets() {
-  //     let res = await fetch('https://swapi.dev/api/planets/?=format=json');
-  //     let data = await res.json();
-  //     setPlanets(data.results);
-  //   }
-
-  //   // fetchPeople();
-  //   fetchPlanets();
-
-  // }, [])
-
-  // console.log('people', people);
-  // console.log('planets', planets);
-
   return (
       <Router>
         <Navbar/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { CardHeader, CardBody, Grid } from './Styles/Card.style';
 
 export default function Home() {
   return (
-    <div>
-      <p>Home Component</p>
-    </div>
+    <>
+      <Grid>
+      <CardHeader>
+        <h2> The Star Wars API </h2>
+        </CardHeader>
+        <CardHeader>
+        <strong> What is this? </strong>
+        </CardHeader>
+        <CardBody>
+          <p>
+          The Star Wars API, or "swapi" (Swah-pee) is the world's first quantified and programmatically-accessible data 
+          source for all the data from the Star Wars canon universe!
+          </p>
+          <p>
+          We've taken all the rich contextual stuff from the universe and formatted into something easier to consume with software. 
+          Then we went and stuck an API on the front so you can access it all!
+          </p>
+          <p>
+          This project is open source and you can contribute on GitHub.
+          </p>
+        </CardBody>
+        <CardHeader>
+        <strong> (what happened to swapi.co?) </strong>
+        </CardHeader>
+        <CardBody>
+          <p>
+          All the Star Wars data you've ever wanted:
+          Planets, Spaceships, Vehicles, People, Films and Species
+          </p>
+          <p>
+          This project is open source and you can contribute on GitHub.
+          </p>
+        </CardBody>
+      </Grid>
+    </>
   )
 }
